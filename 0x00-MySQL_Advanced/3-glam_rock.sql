e43a453367952a3a6adc326dda5416ebabe2f630
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-14 03:05:33 +0300

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,5 +3,5 @@
 SELECT band_name,
        IF(split > 2022, 2022 - formed, COALESCE(split, 2022) - formed) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
