db32ea8f8e567fa59704d0f97ca7254ed6cce531
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-14 05:18:07 +0300

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,31 +1,19 @@
 -- creates a stored procedure that computes and store the average weighted score for a student.
-
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser()
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+    IN p_user_id INT
+)
 BEGIN
-    -- Create temporary table to store computed average scores
-    CREATE TEMPORARY TABLE temp_average_scores (
-        user_id INT,
-        average_score FLOAT
-    );
-
-    -- Insert computed average scores into temporary table
-    INSERT INTO temp_average_scores (user_id, average_score)
-    SELECT c.user_id, AVG(p.weight * c.score) AS average_score
+    DECLARE average_score FLOAT;
+    SELECT AVG(p.weight * c.score) INTO average_score
     FROM corrections c
     JOIN projects p ON c.project_id = p.id
-    GROUP BY c.user_id;
-
-    -- Update users table with computed average scores
-    UPDATE users u
-    JOIN temp_average_scores tas ON u.id = tas.user_id
-    SET u.average_score = tas.average_score;
-
-    -- Drop temporary table after use
-    DROP TEMPORARY TABLE IF EXISTS temp_average_scores;
+    WHERE c.user_id = p_user_id;
+    UPDATE users
+    SET average_score = average_score
+    WHERE id = p_user_id;
+    
 END $$
 
 DELIMITER ;
