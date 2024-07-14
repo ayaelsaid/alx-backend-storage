3ff4ea0a4aaaecd6aa9ef582d23980e380ed0b75
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-14 04:35:51 +0300

    Update 11-need_meeting.sql

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,4 +3,4 @@ CREATE VIEW need_meeting AS
 SELECT *
 FROM students
 WHERE score < 80
-  AND (last_meeting IS NULL OR DATEDIFF(NOW(), last_meeting) > 30);
+  AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
