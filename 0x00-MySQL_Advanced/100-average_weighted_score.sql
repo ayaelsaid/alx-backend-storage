8615840d47ed17f5c0511f5ce9cf902c425b4ec9
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-14 05:08:41 +0300

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,15 +6,19 @@ DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
+    CREATE TEMPORARY TABLE temp_average_scores (
+        user_id INT,
+        average_score FLOAT
+    );
+    INSERT INTO temp_average_scores (user_id, average_score)
+    SELECT c.user_id, AVG(p.weight * c.score) AS average_score
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    GROUP BY c.user_id;
     UPDATE users u
-    JOIN (
-        SELECT u.id, AVG(c.score * p.weight) / AS weight_avg
-        FROM users u
-        JOIN corrections c ON u.id = c.user_id
-        JOIN projects p ON c.project_id = p.id
-        GROUP BY u.id
-    ) AS weighted_avg ON u.id = weighted_avg.id
-    SET u.average_score = weighted_avg.weight_avg;
+    JOIN temp_average_scores tas ON u.id = tas.user_id
+    SET u.average_score = tas.average_score;
+    DROP TEMPORARY TABLE IF EXISTS temp_average_scores;
 END $$
 
 DELIMITER ;
